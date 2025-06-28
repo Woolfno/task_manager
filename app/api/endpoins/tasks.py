959040ff77577ef842236fd8ea4fc0f6921fcd41
Woolf <woolf.no1@gmail.com>
2025-06-28 10:38:38 +0300

    created connection manager for websockets

--- app/api/endpoins/tasks.py
+++ app/api/endpoins/tasks.py
@@ -1,67 +1,69 @@
+from typing import Annotated
 from uuid import UUID
+from json import JSONDecodeError
 
-from fastapi import WebSocket, WebSocketDisconnect
+from fastapi import Depends, WebSocket, WebSocketDisconnect
 from fastapi import status as status_code
 from fastapi.exceptions import HTTPException
 from fastapi.routing import APIRouter
+from pydantic import ValidationError
 
-from app.api.schemas.task import Status, Task, TaskIn
 from app.api.schemas.message import Notification, TypeMsg
-
+from app.api.schemas.task import Status, Task, TaskIn
+from app.api.services.task import TaskService, get_task_service
+from app.api.services.ws import ConnectionManager
 
 router = APIRouter(prefix="/tasks", tags=["Tasks",])
 
-store: dict[UUID, Task] = {}
-
-connection_clients: list[WebSocket] = []
+ws_manager = ConnectionManager()
 
 
 @router.get("/")
-async def list_tasks() -> list[Task]:
-    return [t for t in store.values()]
+async def list_tasks(service: Annotated[TaskService, Depends(get_task_service)]) -> list[Task]:
+    return await service.list()
 
 
 @router.post("/")
-async def create_task(task: TaskIn):
-    t = Task(**task.model_dump())
-    store[t.id] = t
+async def create_task(task: TaskIn, service: Annotated[TaskService, Depends(get_task_service)]):
+    t = await service.create(task)
     msg = Notification(type_msg=TypeMsg.CREATE, task=t)
-    await broadcast_message(msg)
+    await ws_manager.broadcast(msg)
     return t
 
 
 @router.put('/{id}')
-async def change_status(id: UUID, status: Status) -> Task:
-    t = store.get(id)
+async def change_status(id: UUID, status: Status, service: Annotated[TaskService, Depends(get_task_service)]) -> Task:
+    t = await service.update_status(id, status)
     if t is None:
         raise HTTPException(status_code=status_code.HTTP_404_NOT_FOUND)
-    t.status = status
     msg = Notification(type_msg=TypeMsg.UPDATE, task=t)
-    await broadcast_message(msg)
+    await ws_manager.broadcast(msg)
     return t
 
-@router.delete('/{id}')
-async def remove(id:UUID):
-    t = store[id]
-    msg = Notification(type_msg=TypeMsg.DELETE, task=t)
-    await broadcast_message(msg)
-    del store[id]
 
-async def broadcast_message(msg:Notification):
-    for client in connection_clients:
-        await client.send_json(msg.model_dump_json())
+@router.delete('/{id}')
+async def remove(id: UUID, service: Annotated[TaskService, Depends(get_task_service)]):
+    t = await service.get(id)
+    if t is None:
+        return
+    await service.remove(id)
+    msg = Notification(type_msg=TypeMsg.DELETE, task=t)
+    await ws_manager.broadcast(msg)
 
 
 @router.websocket("/ws")
 async def websocket_endpoint(ws: WebSocket):
-    await ws.accept()
-    connection_clients.append(ws)
+    await ws_manager.connect(ws)
     try:
         while True:
-            data = await ws.receive_json()
-
-            for client in connection_clients:
-                if client is not ws:
-                    await client.send_json(data)
+            try:
+                data = await ws.receive_json()
+            except JSONDecodeError:
+                continue
+            try:
+                msg = Notification.model_validate_json(data)
+            except ValidationError:
+                continue
+            await ws_manager.broadcast(msg, ws)
     except WebSocketDisconnect:
-        connection_clients.remove(ws)
+        await ws_manager.disconnect(ws)
---
+++ app/api/services/ws.py
@@ -0,0 +1,19 @@
+from fastapi import WebSocket
+from app.api.schemas.message import Notification
+
+class ConnectionManager:
+    def __init__(self) -> None:
+        self._active_clients:list[WebSocket] = []
+
+    async def connect(self, ws:WebSocket):
+        await ws.accept()
+        self._active_clients.append(ws)
+
+    async def disconnect(self, ws:WebSocket):
+        self._active_clients.remove(ws)
+
+    async def broadcast(self, msg:Notification, me:WebSocket=None): # type: ignore
+        for client in self._active_clients:
+            if client is not me:
+                await client.send_json(msg.model_dump_json())
+                
